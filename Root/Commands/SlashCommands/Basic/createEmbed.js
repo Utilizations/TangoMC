e28b5614f73b7da25a0f28f27ba8131f35e7e12d
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-04 21:16:41 +0100

    Create createEmbed.js

---
+++ Root/Commands/SlashCommands/Basic/createEmbed.js
@@ -0,0 +1,124 @@
+const { Client, CommandInteraction, MessageEmbed, Message } = require("discord.js");
+const config = require("../../../../Config");
+
+module.exports = {
+    name: "createEmbed",
+    description: "fetch's the id of target member.",
+    options: [
+        {
+            name: "title",
+            description: "Embed Title",
+            type: "STRING",
+            required: true,
+        },
+        {
+            name: "description",
+            description: "Embed Description",
+            type: "STRING",
+            required: true,
+        },
+        {
+            name: "footer",
+            description: "Embed Footer",
+            type: "STRING",
+            required: true,
+        },
+        {
+            name: "options",
+            description: "Embed Options",
+            type: "STRING",
+            required: true,
+            options: [
+                {
+                    name: "logo",
+                    description: "Would you like the logo? (y/n)",
+                    type: "STRING",
+                },
+                {
+                    name: "banner",
+                    description: "Would you like the logo? (y/n)",
+                    type: "STRING",
+                },
+            ]
+        }
+    ],
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+     run: async(client, interaction, container) => {
+        //const choices = interaction.options.getString("member");
+        const Logo = interaction.options.getString("logo");
+        const Title = interaction.options.getString("title");
+        const Description = interaction.options.getString("description");
+        const Footer = interaction.options.getString("footer") || "N/A";
+        const Banner = interaction.options.getString("banner");
+
+        if(Footer === "N/A") {
+            if(Logo === "y"){
+                if(Banner === "y") {
+                    const embed1 = new MessageEmbed()
+                    .setTitle(Title)
+                    .setDescription(Description)
+                    .setThumbnail(config.serverIcon)
+                    .setImage(config.serverBanner)
+                    interaction.channel.send({embeds: [embed1]})
+                } else {
+                    const embed2 = new MessageEmbed()
+                    .setTitle(Title)
+                    .setDescription(Description)
+                    .setThumbnail(config.serverIcon)
+                    interaction.channel.send({embeds: [embed2]})
+                }
+            } else {
+                if(Banner === "y") {
+                    const embed3 = new MessageEmbed()
+                    .setTitle(Title)
+                    .setDescription(Description)
+                    .setImage(config.serverBanner)
+                    interaction.channel.send({embeds: [embed3]})
+                }else {
+                    const embed4 = new MessageEmbed()
+                    .setTitle(Title)
+                    .setDescription(Description)
+                    interaction.channel.send({embeds: [embed4]})
+                }
+            }
+        }else {
+            if(Logo === "y"){
+                if(Banner === "y") {
+                    const embed5 = new MessageEmbed()
+                    .setTitle(Title)
+                    .setDescription(Description)
+                    .setThumbnail(config.serverIcon)
+                    .setImage(config.serverBanner)
+                    .setFooter(Footer)
+                    interaction.channel.send({embeds: [embed5]})
+                } else {
+                    const embed6 = new MessageEmbed()
+                    .setTitle(Title)
+                    .setDescription(Description)
+                    .setThumbnail(config.serverIcon)
+                    .setFooter(Footer)
+                    interaction.channel.send({embeds: [embed6]})
+                }
+            } else {
+                if(Banner === "y") {
+                    const embed7 = new MessageEmbed()
+                    .setTitle(Title)
+                    .setDescription(Description)
+                    .setImage(config.serverBanner)
+                    .setFooter(Footer)
+                    interaction.channel.send({embeds: [embed7]})
+                }else {
+                    const embed8 = new MessageEmbed()
+                    .setTitle(Title)
+                    .setDescription(Description)
+                    .setFooter(Footer)
+                    interaction.channel.send({embeds: [embed8]})
+                }
+            }
+        }
+    }
+}
