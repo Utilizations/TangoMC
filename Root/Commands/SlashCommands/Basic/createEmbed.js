2d4ab9e2fa95d3a6af3a398320441127e48402fd
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-04 21:17:47 +0100

    Update createEmbed.js

--- Root/Commands/SlashCommands/Basic/createEmbed.js
+++ Root/Commands/SlashCommands/Basic/createEmbed.js
@@ -26,7 +26,6 @@ module.exports = {
         {
             name: "options",
             description: "Embed Options",
-            type: "STRING",
             required: true,
             options: [
                 {
