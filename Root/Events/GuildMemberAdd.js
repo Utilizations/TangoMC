5a4955392794342f2d3b2ec8a8fccff4624ed132
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-04 20:53:23 +0100

    Update GuildMemberAdd.js

--- Root/Events/GuildMemberAdd.js
+++ Root/Events/GuildMemberAdd.js
@@ -1,34 +1,68 @@
 const discord = require("discord.js")
 const config = require("../../Config")
-const invites = new discord.Collection();
+const InvitesTracker = require('@androz2091/discord-invites-tracker');
+const tracker = InvitesTracker.init(client, {
+    fetchGuilds: true,
+    fetchVanity: true,
+    fetchAuditLogs: true
+});
 
 module.exports = {
     name: "guildMemberAdd",
     run: async(guildMember) => {
-        const newInvites = await guildMember.guild.invites.fetch()
-        // This is the *existing* invites for the guild.
-        const oldInvites = invites.get(guildMember.guild.id);
-        // Look through the invites, find the one for which the uses went up.
-        const invite = newInvites.find(i => i.uses > oldInvites.get(i.code));
-        // This is just to simplify the message being sent below (inviter doesn't have a tag property)
-        const inviter = await client.users.fetch(invite.inviter.id);
-        // Get the log channel (change to your liking)
-        //inviter
-        //    ? logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`)
-        //    : logChannel.send(`${member.user.tag} joined but I couldn't find through which invite.`);
-
-        const welcomeembed = new discord.MessageEmbed()
-        .setAuthor({name: `${config.serverName} Welcome!`, iconURL: config.serverIcon})
-        .setThumbnail(config.serverIcon)
-        .setImage(config.serverBanner)
-        .setColor(config.setColor)
-        .setDescription(`
-        Welcome ${guildMember} to **TangoMC**
-        `)
-        .addField("Invited By:", inter.tag)
-        .addField("Invite Code:", intvite.code, true)
         log1 = guildMember.guild.channels.cache.get(config.welcomeChannel)
-        log1.send({embeds: [welcomeembed]})
         guildMember.roles.add(config.memberRole)
+
+        if(type === 'normal'){
+            const welcomeembed1 = new discord.MessageEmbed()
+            .setAuthor({name: `${config.serverName} Welcome!`, iconURL: config.serverIcon})
+            .setThumbnail(config.serverIcon)
+            .setImage(config.serverBanner)
+            .setColor(config.setColor)
+            .setDescription(`
+            Welcome ${guildMember} to **TangoMC**
+            `)
+            .addField("Invited By:", invite.inviter.username)
+            log1.send({embeds: [welcomeembed1]})
+        }
+    
+        else if(type === 'vanity'){
+            const welcomeembed2 = new discord.MessageEmbed()
+            .setAuthor({name: `${config.serverName} Welcome!`, iconURL: config.serverIcon})
+            .setThumbnail(config.serverIcon)
+            .setImage(config.serverBanner)
+            .setColor(config.setColor)
+            .setDescription(`
+            Welcome ${guildMember} to **TangoMC**
+            `)
+            .addField("Invited By:", "Vanity Invite")
+            log1.send({embeds: [welcomeembed2]})
+        }
+    
+        else if(type === 'permissions'){
+            const welcomeembed3 = new discord.MessageEmbed()
+            .setAuthor({name: `${config.serverName} Welcome!`, iconURL: config.serverIcon})
+            .setThumbnail(config.serverIcon)
+            .setImage(config.serverBanner)
+            .setColor(config.setColor)
+            .setDescription(`
+            Welcome ${guildMember} to **TangoMC**
+            `)
+            .addField("Invited By:", "I need MANAGER_SERVER permissions")
+            log1.send({embeds: [welcomeembed3]})
+        }
+    
+        else if(type === 'unknown'){
+            const welcomeembed4 = new discord.MessageEmbed()
+            .setAuthor({name: `${config.serverName} Welcome!`, iconURL: config.serverIcon})
+            .setThumbnail(config.serverIcon)
+            .setImage(config.serverBanner)
+            .setColor(config.setColor)
+            .setDescription(`
+            Welcome ${guildMember} to **TangoMC**
+            `)
+            .addField("Invited By:", "Unknown")
+            log1.send({embeds: [welcomeembed4]})
+        }
     }
 }
