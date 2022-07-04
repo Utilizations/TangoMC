1f8cc4bfb6fd6d6e76525b540e66bde22fd6d575
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-04 17:30:59 +0100

    awdw

---
+++ Root/Commands/SlashCommands/Basic/test.js
@@ -0,0 +1,51 @@
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const config = require("../../../../Config");
+
+module.exports = {
+    name: "test",
+    description: "fetch's the id of target member.",
+    options: [
+        {
+            name: "member",
+            description: "Guild Member Events.",
+            type: "STRING",
+            required: true,
+            choices: [
+                {
+                    name: "guildMemberAdd",
+                    value: "guildMemberAdd"
+                },
+                {
+                    name: "guildMemberRemove",
+                    value: "guildMemberRemove"
+                }
+            ]
+        }
+    ],
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+     run: async(client, interaction, container) => {
+        const choices = interaction.options.getString("member");
+
+        switch(choices) {
+            case "guildMemberAdd" : {
+                client.emit("guildMemberAdd", interaction.member);
+                interaction.reply({content: "Emitted the event.", ephemeral: true})
+            }
+            break;
+            case "guildMemberAdd" : {
+                client.emit("guildMemberAdd", interaction.member);
+                interaction.reply({content: "Emitted the event.", ephemeral: true})
+            }
+            break;
+            case "guildMemberRemove" : {
+            client.emit("guildMemberRemove", interaction.member);
+            interaction.reply({content: "Emitted the event.", ephemeral: true})
+            }
+            break;
+        }
+    }
+}
--- Root/Events/GuildMemberAdd.js
+++ Root/Events/GuildMemberAdd.js
@@ -8,12 +8,12 @@ module.exports = {
         .setAuthor({name: `${config.serverName} Welcome!`, iconURL: config.serverIcon})
         .setThumbnail(config.serverIcon)
         .setImage(config.serverBanner)
-        .setColor(conig.setColor)
+        .setColor(config.setColor)
         .setDescription(`
         Welcome ${guildMember} to **TangoMC**
         `)
-        log1 = message.guild.channels.cache.get(config.welcomeChannel)
-        log.send({embeds: [welcomeembed]})
+        log1 = guildMember.guild.channels.cache.get(config.welcomeChannel)
+        log1.send({embeds: [welcomeembed]})
         guildMember.roles.add(config.memberRole)
     }
 }
