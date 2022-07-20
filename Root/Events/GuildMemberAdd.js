d63dee3ba162861927d7c72d146dc9021920d9ce
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-20 12:02:43 +0100

    Update GuildMemberAdd.js

--- Root/Events/GuildMemberAdd.js
+++ Root/Events/GuildMemberAdd.js
@@ -5,22 +5,7 @@ const db = require("../Structures/Models/InvitesDB")
 module.exports = {
     name: "guildMemberAdd",
     run: async(member) => {
-        let invites = await member.guild.invites.fetch();
-        let userInv = invites.filter(u => u.inviter && u.inviter.id === invites.inviter.id)
 
-        db.findOne({ GuildID: message.guild.id, UserID: userInv}, async (err, data) => {
-            if(err) throw err;
-            if(data) {
-                data.updateOne({Invites: data.Invites + 1})
-            }else {
-                const obj = {
-                    VoterID: 1,
-                }
-                data.Invites.push(obj)
-                data.save()
-            }
-        })
-        
         const welcomeembed = new discord.MessageEmbed()
         .setAuthor({name: `${config.serverName} Welcome!`, iconURL: config.serverIcon})
         .setThumbnail(config.serverIcon)
