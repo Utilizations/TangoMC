0c445098516b919f6cc6449362b39ff4ef01d6b0
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-23 20:01:21 +0100

    dwad

---
+++ Root/Commands/ButtonCommands/about/forums.js
@@ -0,0 +1,11 @@
+const config = require("../../../Config");
+const Discord = require("discord.js")
+const db = require("../../Structures/Models/ClaimDB");
+
+module.exports = {
+    name : 'forums',
+    run : async(client, interaction, container) => {
+        const { guild, member} = interaction;
+        interaction.reply({content: "---", ephemeral: true})
+    }
+}
---
+++ Root/Commands/ButtonCommands/about/serverip.js
@@ -0,0 +1,12 @@
+const config = require("../../../Config");
+const Discord = require("discord.js")
+const db = require("../../Structures/Models/ClaimDB");
+
+module.exports = {
+    name : 'ip',
+    run : async(client, interaction, container) => {
+        const { guild, member} = interaction;
+        interaction.reply({content: "play.AhertMC.net", ephemeral: true})
+
+    }
+}
