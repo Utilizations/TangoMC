f3423633d182c54c96036b1b28b8794c5c3fd1f9
Utilizations <Utilizationsbusiness@gmail.com>
2022-07-23 20:02:00 +0100

    wda

--- Root/Commands/ButtonCommands/about/serverip.js
+++ Root/Commands/ButtonCommands/about/serverip.js
@@ -1,4 +1,4 @@
-const config = require("../../../Config");
+const config = require("../../../../Config");
 const Discord = require("discord.js")
 const db = require("../../Structures/Models/ClaimDB");
 
